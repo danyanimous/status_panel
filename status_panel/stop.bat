efd7ace0dde014777b938ddaa2af0a17a6d347e9
danyanimous <projector.master.uss17@gmail.com>
2024-05-26 19:53:08 +1000

    агде

--- status_panel/stop.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-@taskkill /pid httpd.exe
-@taskkill /pid status_panel.exe
--- status_panel/web/index.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const text = await (await fetch("status.txt")).text();
-	setTimeout(() => {
-		document.getElementById("text").innerHTML = text; 
-	}, 8);
-})();
-
